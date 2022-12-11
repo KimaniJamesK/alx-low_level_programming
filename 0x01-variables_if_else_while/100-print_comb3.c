da0ac061aad7b2e56a96e46567eb08da6c1dffcf
KimaniJamesK <kimanijames941@gmail.com>
2022-12-11 11:30:31 -0800

    inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,13 +6,15 @@
  *
  * The two digits must be different
  *
- * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * 01 and 10 are considered the same 
+ * combination of the two digits 0 and 1
  *
  * Print only the smallest combination of two digits
  *
  * Numbers should be printed in ascending order, with two digits
  *
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use the putchar function 
+ * (every other function (printf, puts, etc…) is forbidden)
  *
  * You can only use putchar five times maximum in your code
  *
