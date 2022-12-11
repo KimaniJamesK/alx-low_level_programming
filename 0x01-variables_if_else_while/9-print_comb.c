8edc44364def2f1c0ceaa5b1c4f6ed5eb8feae3f
KimaniJamesK <kimanijames941@gmail.com>
2022-12-11 12:43:20 -0800

    ppp

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,7 +5,7 @@
  * main - main block
  * Description: prints all single numbers of base 10
  * starting from 0, followed by a new line
- * Return: Always 0 (succes)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
