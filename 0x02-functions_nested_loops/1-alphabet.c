2ae5151944ea25ecedaf8a1bd0d654dacb425edb
KimaniJamesK <kimanijames941@gmail.com>
2022-12-14 17:48:55 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 0; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
