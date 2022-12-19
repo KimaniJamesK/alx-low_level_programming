b662cb2fc79adc44355d1d55f41b9fa4b2af8710
KimaniJamesK <kimanijames941@gmail.com>
2022-12-19 11:59:08 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int length =0;
+	int o;
+
+	while (*s != '\n')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (o = length; o > 0; o--);
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
