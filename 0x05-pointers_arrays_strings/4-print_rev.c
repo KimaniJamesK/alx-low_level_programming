8fcbafd754b0d33717fc7de579c3231d879df4ab
KimaniJamesK <kimanijames941@gmail.com>
2022-12-19 12:18:52 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int length = 0;
-	int o;
+	int index;
 
-	while (*s != '\n')
+	for (index =0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
-		length++;
-		s++;
-	}
-	s--;
-	for (o = length; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
+		_putchar(s[index]);
 	}
 	_putchar('\n');
 }
