51589255ae94ecea4a96c28c4f80922492aa1935
KimaniJamesK <kimanijames941@gmail.com>
2023-01-05 07:19:20 -0800

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	char n;
+	return 0;
+}
+int main(void)
+{
+	printf("The string is %d", n'\n');
+	return 0;
+}
