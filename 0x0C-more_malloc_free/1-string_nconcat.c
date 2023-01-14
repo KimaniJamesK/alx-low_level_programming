1a80fb4a451a2ced283f3cbffd4b82d168d941c8
KimaniJamesK <kimanijames941@gmail.com>
2023-01-14 07:30:42 -0800

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2++;
 	}
 	if (n > size2)
-		n = size2;
+	n = size2;
 	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s1[i];
 	}
-	for (i = 0; i < (size1 + n); i++)
+	for (i < (size1 + n); i++)
 	{
 		p[i] = s2[i - size1];
 	}
