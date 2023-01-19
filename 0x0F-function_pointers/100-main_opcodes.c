c6fcea29dc1e74e67b4a8dead3f08d3c37aadf55
KimaniJamesK <kimanijames941@gmail.com>
2023-01-19 07:20:01 -0800

    f pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
@@ -23,6 +24,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+
 	arr = (char *)main;
 
 	for (i = 0; i < bytes; i++)
