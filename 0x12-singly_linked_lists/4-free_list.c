f447b89a676d6e6ea25c0b001913a1bf45c5bc3f
KimaniJamesK <kimanijames941@gmail.com>
2023-01-30 11:24:48 -0800

    singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * free_list - Frees list_t
+ * @head: pointer to list_t
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
